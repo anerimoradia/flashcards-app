579be3d9b65f2fd24e656127b6c0f506c9867d67
anerimoradia <anerimoradia@gmail.com>
2026-01-16 15:51:52 -0800

    creating piece of state called flipped

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -1,21 +1,35 @@
-function Flashcard ({ question, answer}) {
+import { useState } from 'react';
+
+function Flashcard({ question, answer }) {
+    const [flipped, setIsFlipped] = useState(false);
+
     return(
         <div
-        style={{
-            border: "1px solid #ccc",
-            borderRadius: "8px",
-            padding: "1rem",
-            maxWidth: "600px",
-            marginTop: "1rem"
-        }}
+            onClick={() => setIsFlipped(!isFlipped)}
+            style={{
+                border: "1px solid #ccc",
+                borderRadius: "8px",
+                padding: "1rem",
+                maxWidth: "300px",
+                marginTop: "1rem",
+                cursor: "pointer",
+                background: flipped ? "#222" : "#fff",
+                color: flipped ? "#fff" : "#000",
+                transition: "0.3s ease"
+            }}
         >
-            <h2>Question:</h2>
-            <p>{question}</p>
-
-            <h2>Answer:</h2>
-            <p>{answer}</p>
+            {flipped ? (
+                <>
+                    <h2>Answer</h2>
+                    <p>{answer}</p>
+                </>
+            ) : (
+                <>
+                    <h2>Question</h2>
+                    <p>{question}</p>
+                </>
+            )}
         </div>
     );
 }
-
 export default Flashcard;
