583946494b407ee1e821f5a3e048a61d34d6f693
anerimoradia <anerimoradia@gmail.com>
2026-01-18 21:44:51 -0800

    Refactor App component to use an array of flashcards for better scalability and maintainability

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import Flashcard from "./Flashcard";
 
 function App() {
-  const question = "What is a closure in JavaScript?";
-  const answer = "A closure is a function that remembers its outer variables even after the outer function has executed.";
+
+  const flashcards = [
+    {
+      question: "What is a closure in JavaScript?",
+      answer: "A closure is a function that remembers its outer variables even after the outer function has executed."
+    },
+    {
+      question: "What is a closure in JavaScript?",
+      answer: "A closure is a function that remembers its outer variables even after the outer function has executed."
+    },
+    {
+      question: "What is a closure in JavaScript?",
+      answer: "A closure is a function that remembers its outer variables even after the outer function has executed."
+    }
+  ];
+
+  const currentcard = flashcards[0];
 
   return (
     <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
       <h1>JavaScript Flashcards</h1>
 
       <Flashcard 
-        question={question}
-        answer={answer}
+        question={currentcard.question}
+        answer={currentcard.answer}
       />
     </div>
   );
